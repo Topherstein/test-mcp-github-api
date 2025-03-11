0bfa452dc5e7b5532c4c8a3f6f921da4a96ce782
Topherstein <ksteinke83@gmail.com>
2025-03-10 22:27:27 -0400

    Add test JavaScript file

---
+++ test-file.js
@@ -0,0 +1,8 @@
+// This is a test JavaScript file
+console.log('Hello from GitHub MCP API!');
+
+function testFunction() {
+  return 'Testing GitHub MCP API functionality';
+}
+
+module.exports = testFunction;
